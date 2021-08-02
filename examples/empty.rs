9ed22cec413ac4f31bc4e608c2753f2a430ad625
Robert Wiewel <dev@ducktec.de>
2021-08-02 08:32:24 +0200

    Update empty example to disable watchdogs

--- examples/empty.rs
+++ examples/empty.rs
@@ -1,13 +1,25 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
-extern crate riscv_rt;
-
+#[allow(unused_imports)]
+use panic_halt;
 use riscv_rt::entry;
+
+extern crate esp32c3_hal;
+
+use esp32c3_hal::{pac, prelude::*, RtcCntl, Timer};
 
 #[entry]
 fn main() -> ! {
+    let peripherals = pac::Peripherals::take().unwrap();
+
+    let rtccntl = RtcCntl::new(peripherals.RTCCNTL);
+    let mut timer = Timer::new(peripherals.TIMG0);
+
+    rtccntl.set_super_wdt_enable(false);
+    rtccntl.set_wdt_enable(false);
+    timer.disable();
+
     // do nothing here
     loop {}
 }
