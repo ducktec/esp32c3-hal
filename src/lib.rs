c944615a089cd99387c23b1afc6df2243ab5c7a9
Robert Wiewel <dev@ducktec.de>
2021-08-08 21:30:31 +0200

    Implement basic functionality for Serial

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,10 @@ pub use esp32c3 as pac;
 
 pub mod control;
 pub mod prelude;
+pub mod serial;
 pub mod timer;
 pub mod units;
 
 pub use crate::control::RtcCntl;
+pub use crate::serial::Serial;
 pub use crate::timer::Timer;
